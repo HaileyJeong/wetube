6fa04a8f2067dc1d2aae1e83e4039bd076fdf852
HaileyJeong <115683734+HaileyJeong@users.noreply.github.com>
2023-01-29 22:45:07 +0900

    first-submit

---
+++ src/models/Video.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const videoShema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 10 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
+  comments: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+});
+
+videoShema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+const Video = mongoose.model("Video", videoShema);
+
+export default Video;
