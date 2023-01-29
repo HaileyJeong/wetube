54855f78baaf11c9d7d964b7043ad6e9cbcdb9b1
HaileyJeong <115683734+HaileyJeong@users.noreply.github.com>
2023-01-30 05:10:05 +0900

    revision

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -167,7 +167,11 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
+      avatarUrl: file
+        ? isHeroku
+          ? file.location
+          : file.path
+        : "/" + avatarUrl,
       name,
       email,
       username,
